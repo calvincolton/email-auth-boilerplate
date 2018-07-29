d5eef0943ff20de0c6015bb12d730e2dd86cf1ff
Calvin Colton <calvincolton@gmail.com>
2018-07-29 16:47:17 -0700

    SIGN IN - passport local strategy, route, user comparePassword method and authentication routing

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt-nodejs');
 
-// Define our model
 const userSchema = new Schema({
   email: {
     type: String,
@@ -12,27 +11,29 @@ const userSchema = new Schema({
   password: String
 });
 
-// On Save hook, encrypt password
 userSchema.pre('save', function(next) {
   const user = this;
 
-  // generate a salt, then run callback
   bcrypt.genSalt(10, function(err, salt) {
     if (err) { return next(err); }
 
-    // hash (encrypt) our password, using our salt
     bcrypt.hash(user.password, salt, null, function(err, hash) {
       if (err) { return next(err); }
 
-      // overwrite plain text password with encrypted password
       user.password = hash;
       next();
     });
   })
 });
 
-// Create a model class
+userSchema.methods.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) { return callback(err); }
+
+    callback(null, isMatch);
+  });
+}
+
 const ModelClass = mongoose.model('user', userSchema);
 
-// Export the model
 module.exports = ModelClass;
